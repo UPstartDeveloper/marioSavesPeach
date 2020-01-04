2e37d7d9433960f86958a66ccc9e841945518d22
= <zainr7989@gmail.com>
2020-01-04 12:22:23 -0500

    Implement savePrincess method.

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class marioSavesPrincess
 {
     /*
      * Pre:
-     *     int n: an integer that stores the length of the maze, a square grid
+     *     int rowLength: an integer that stores the length of the maze, a square grid
      *     String[] maze: an array of Strings, which represent the rows of the maze.
      *                    "-" represents a step Mario can take, "m" is Mario, "p" is
      *                     Princess Peach.
@@ -12,9 +12,50 @@ public class marioSavesPrincess
      *     String[] instructions: an array of instructions for Mario, so here can find
      *                            Princess Peach.
      */
-    public static String[] savePrincess(int n, String[] maze)
+    public static String[] savePrincess(int rowLength, String[] maze)
     {
-        String[] instructions = new String[n];
+        // figure out where Mario is
+        int mazeSizeHalved = rowLength % 2;
+        int[] marioLoc = { mazeSizeHalved, mazeSizeHalved };
+        // declare array for storing instructions for Mario
+        String[] instructions = new String[rowLength - 1];
+        // figure out where the Princess is
+        int[] princessLoc = new int[2];
+        for(int i = 0; i < maze.length; i++)
+        {
+            String row = maze[i];
+            for(int j = 0; j < row.length(); j++)
+            {
+
+                if(row.substring(j, j + 1).equals("p"))
+                {
+                    princessLoc[0] = i;
+                    princessLoc[1] = j;
+                }
+            }
+        }
+        // populate instructions array appropiately
+        for(int i = 0; i < mazeSizeHalved; i += 2)
+        {
+            // determine the direction Mario must go across columns
+            if (marioLoc[0] > princessLoc[0])
+            {
+                instructions[i] = "UP";
+            }
+            else
+            {
+                instructions[i] = "DOWN";
+            }
+            // determine the direction Mario must go across rows
+            if (marioLoc[1] > princessLoc[1])
+            {
+                instructions[i + 1] = "LEFT";
+            }
+            else
+            {
+                instructions[i + 1] = "RIGHT";
+            }
+        }
         return instructions;
     }
 

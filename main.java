8109af133f317059e291fbecfe218bba45a57762
= <zainr7989@gmail.com>
2020-01-04 11:28:18 -0500

    [Add] Method declarations, pre and post condition.

---
+++ main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class marioSavesPrincess
+{
+    /*
+     * Pre:
+     *     int n: an integer that stores the length of the maze, a square grid
+     *     String[] maze: an array of Strings, which represent the rows of the maze.
+     *                    "-" represents a step Mario can take, "m" is Mario, "p" is
+     *                     Princess Peach.
+     * Post:
+     *     String[] instruct: an array of instructions for Mario, so here can find
+     *                        Princess Peach.
+     */
+    public static String[] savePrincess(int n, String[] maze)
+    {
+
+    }
+    public static void main(String[] args)
+    {
+        // take input
+        // calculate instructions for reaching the princess
+        // display instructions
+    }
+}
